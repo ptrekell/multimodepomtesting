5bf2301d50eded80cc23b350a167eb7539d0781a
P Trekell <pavel.trekell@gmail.com>
2018-12-19 14:19:49 -0800

    initial commit

---
+++ testing-the-spring-way/src/main/java/com/pt/testing/springway/testingthespringway/TestingTheSpringWayApplication.java
@@ -0,0 +1,14 @@
+package com.pt.testing.springway.testingthespringway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTheSpringWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingTheSpringWayApplication.class, args);
+    }
+
+}
+
---
+++ testing-the-spring-way/src/test/java/com/pt/testing/springway/testingthespringway/TestingTheSpringWayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pt.testing.springway.testingthespringway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestingTheSpringWayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
